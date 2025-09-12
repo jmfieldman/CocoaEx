a67c2ca9fe65a6fd54db4c428a4a1e96ea87cdbc
Jason Fieldman <jason@fieldman.org>
2025-09-12 07:18:24 -0700

    comments

--- Sources/Foundation/Locale+Extensions.swift
+++ Sources/Foundation/Locale+Extensions.swift
@@ -5,6 +5,11 @@
 
 import Foundation
 
+/// A set of extensions to the Locale type.
 public extension Locale {
+    /// A static property that provides an English (United States) locale.
+    ///
+    /// This property returns a Locale instance configured for English (United States)
+    /// which can be used for consistent localization for internal enum cases.
     static let enUS = Locale(identifier: "en_US")
 }
--- Sources/Foundation/RawRepresentable+Extensions.swift
+++ Sources/Foundation/RawRepresentable+Extensions.swift
@@ -5,7 +5,12 @@
 
 import Foundation
 
+/// A collection of extensions for types conforming to `RawRepresentable`.
 public extension RawRepresentable {
+    /// Initializes a new instance of the conforming type using an optional raw value.
+    ///
+    /// - Parameter optionalRawValue: The raw value to use for initialization, or `nil` if the instance should not be initialized.
+    /// - Returns: A new instance of the conforming type if the raw value is valid, otherwise `nil`.
     init?(optionalRawValue: RawValue?) {
         guard let optionalRawValue else {
             return nil
@@ -14,7 +19,15 @@ public extension RawRepresentable {
     }
 }
 
+/// A collection of case-insensitive matching extensions for `RawRepresentable` types that also conform to `CaseIterable` and have a `String` raw value.
 public extension RawRepresentable where RawValue == String, Self: CaseIterable {
+    /// Attempts to find a case-insensitive match for a given string among the cases of the conforming type.
+    ///
+    /// This method first attempts an exact match, then tries lowercased and uppercased versions of the needle string.
+    /// If those fail, it performs a case-insensitive search through all cases.
+    ///
+    /// - Parameter needle: The string to match against the cases, or `nil` if no match should be attempted.
+    /// - Returns: The matching case if found, otherwise `nil`.
     static func caseInsensitive(matching needle: String?) -> Self? {
         guard let needle else {
             return nil
