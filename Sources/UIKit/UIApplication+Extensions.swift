52bfd1f35b768987459a5e1025c692a06f98f96c
Jason Fieldman <jason@fieldman.org>
2025-12-29 07:20:59 -0800

    resignAnyFirstResponder

---
+++ Sources/UIKit/UIApplication+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIApplication+Extensions.swift
+//  Copyright © 2025 Jason Fieldman.
+//
+
+import UIKit
+
+public extension UIApplication {
+    /// Sends the UIResponder.resignFirstResponder to the first responder in the UIApplication.
+    func resignAnyFirstResponder() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
