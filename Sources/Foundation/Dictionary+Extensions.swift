21b8c4f9c2d912c0ff311126681f8554171acec0
Jason Fieldman <jason@fieldman.org>
2026-01-16 08:04:30 -0800

    add dictionary map

--- Sources/Foundation/Dictionary+Extensions.swift
+++ Sources/Foundation/Dictionary+Extensions.swift
@@ -19,4 +19,19 @@ public extension Dictionary {
         modifications(&copied)
         return copied
     }
+
+    /// Maps the dictionary to an array based on the specific provided key
+    /// order. Entries in the dictionary not represented in the keyOrder will
+    /// not be present in the result. Can be used in conjunction with
+    /// `mapToGroupedDictionary` to convert an array of random elements into
+    /// a deterministically-ordered array of sections.
+    func compactMapWithKeyOrder<T>(
+        _ keyOrder: [Key],
+        _ transform: (Key, Value) throws -> T?
+    ) rethrows -> [T] {
+        try keyOrder.compactMap { key in
+            guard let value = self[key] else { return nil }
+            return try transform(key, value)
+        }
+    }
 }
