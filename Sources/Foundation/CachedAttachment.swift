3fad85f2369b41e6e60c3c21e73a4ea4a1effe8e
Jason Fieldman <jason@fieldman.org>
2026-01-27 16:22:53 -0800

    cached attachment

---
+++ Sources/Foundation/CachedAttachment.swift
@@ -0,0 +1,31 @@
+//
+//  CachedAttachment.swift
+//  Copyright © 2026 Jason Fieldman.
+//
+
+public protocol CachedObjectAttaching: AnyObject {
+    func cachedAttachment<T>(key: inout Int, factory: () -> T?) -> T?
+}
+
+public extension CachedObjectAttaching {
+    func cachedAttachment<T>(key: inout Int, factory: () -> T) -> T {
+        objc_sync_enter(self)
+        defer { objc_sync_exit(self) }
+
+        if let cacheBox = objc_getAssociatedObject(self, &key) as? CachedAttachmentBox<T> {
+            return cacheBox.value
+        }
+
+        let newValue = factory()
+        let cacheBox = CachedAttachmentBox(newValue)
+        objc_setAssociatedObject(self, &key, cacheBox, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        return newValue
+    }
+}
+
+class CachedAttachmentBox<T> {
+    let value: T
+    init(_ value: T) {
+        self.value = value
+    }
+}
