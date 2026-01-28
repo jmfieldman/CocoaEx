473c61a1dacab8cedf55c01fc7def431386fc630
Jason Fieldman <jason@fieldman.org>
2026-01-27 16:28:33 -0800

    with input

--- Sources/Foundation/CachedAttachment.swift
+++ Sources/Foundation/CachedAttachment.swift
@@ -5,6 +5,7 @@
 
 public protocol CachedObjectAttaching: AnyObject {
     func cachedAttachment<T>(key: inout Int, factory: () -> T?) -> T?
+    func cachedAttachment<T, I>(key: inout Int, input: I, factory: (I) -> T?) -> T?
 }
 
 public extension CachedObjectAttaching {
@@ -17,6 +18,20 @@ public extension CachedObjectAttaching {
         }
 
         let newValue = factory()
+        let cacheBox = CachedAttachmentBox(newValue)
+        objc_setAssociatedObject(self, &key, cacheBox, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        return newValue
+    }
+
+    func cachedAttachment<T, I>(key: inout Int, input: I, factory: (I) -> T) -> T {
+        objc_sync_enter(self)
+        defer { objc_sync_exit(self) }
+
+        if let cacheBox = objc_getAssociatedObject(self, &key) as? CachedAttachmentBox<T> {
+            return cacheBox.value
+        }
+
+        let newValue = factory(input)
         let cacheBox = CachedAttachmentBox(newValue)
         objc_setAssociatedObject(self, &key, cacheBox, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         return newValue
